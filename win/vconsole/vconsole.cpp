7420268a291b79e72858be8793b72edd1228e1ec
fells <fellipersv@live.com>
2017-09-24 23:19:07 -0300

    Added visual studio 2017 project

---
+++ win/vconsole/vconsole.cpp
@@ -0,0 +1,38 @@
+// vconsole.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+
+// TO-DO List
+// 1 - Create a output idle screen like "vconsole>"
+// 2 - Make program read a user input 
+// 3 - Make program exit with quit command or ctrl+c
+//
+
+
+
+int main()
+{
+    return 0;
+}
+/*
+------------------------------------------------------------------------------------------
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+|vconsole>exit
+|vconsole>quit
+|vconsole>
+vconsole>
+vconsole>
+
+while()
+------------------------------------------------------------------------------------------
+*/
